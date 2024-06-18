9cfc82b12c9811b0172ce4aecde41a19407186d2
Master23705 <157723597+Master23705@users.noreply.github.com>
2024-06-18 12:45:22 +0200

    Update FUNCIONS.h

--- FUNCIONS.h
+++ FUNCIONS.h
@@ -20,7 +20,8 @@ typedef struct {
 extern Usuari *usuaris;
 extern int *distancies;
 
-int inicializar_datos(Usuari** usuaris, int** distancies, int* num_usuaris);
+int editar_perfil(int id, Usuari * usuaris, int num_usuaris);
+int inicialitzar_dades(Usuari** usuaris, int** distancies, int* num_usuaris);
 int llegir_usuaris(Usuari **usuaris, int *num_usuaris);
 int llegir_distancies(int **distancies, int num_usuaris);
 void mostra_perfil(int id, Usuari *usuaris);
@@ -28,8 +29,9 @@ void mostra_menu();
 void mostrar_amistats(int id, int *distancies, int num_usuaris, Usuari *usuaris);
 void mergeSort(Proper arr[], int l, int r);
 int* usuaris_propers(int id, int *distancies, int num_usuaris, int *count);
-int escriure_en_fitxer(int *distancies, int num_usuaris);
+int escriure_en_fitxer_distancies(int *distancies, int num_usuaris);
 void afegir_amistat(int id, int *distancies, int *ids_propers, int count, int num_usuaris);
 void eliminar_amistat(int id, int *distancies, int num_usuaris);
+int escriure_en_fitxer_usuaris();
 
 #endif
